a9c0fc4437b011811fb1f6438e4375742313fb21
Asish <gitasish1212@gmail.com>
2026-04-07 19:39:26 +0530

    contect form issue testing 3

--- config/mailer.js
+++ config/mailer.js
@@ -1,26 +1,13 @@
-// import nodemailer from "nodemailer";
-
-// const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: "portfolio.p2019@gmail.com",
-//     pass: "hwva wmcg aohu wwod"
-//   }
-// });
-
-// export default transporter;
-
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,          // ✅ IMPORTANT
-  secure: false,      // ✅ IMPORTANT (must be false for 587)
+  service: "gmail",
   auth: {
     user: "portfolio.p2019@gmail.com",
-    pass: "hwva wmcg aohu wwod",
-  },
-  family: 4,          // ✅ FORCE IPv4 (fixes ENETUNREACH)
+    pass: "hwva wmcg aohu wwod"
+  }
 });
 
 export default transporter;
+
+
